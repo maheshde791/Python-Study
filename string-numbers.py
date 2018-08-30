78cef07d9ce8ce1524852f308bb981e5be0915e6
Mahesh Deore <maheshde@cybage.com>
2018-08-30 17:25:14 +0530

    added numbers and strings file

---
+++ string-numbers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 30 17:13:43 2018
+
+@author: maheshde
+
+Strings and Numbers
+You can declare strings in either single or double quotes
+"""
+
+year = "1991"
+actyear=1991
+print(actyear,year)
+
+actyear=++actyear
+year=year+1 
+""" You will get error here that it is string it canbe be concatenated but 
+you cant treat it like an int
+"""
