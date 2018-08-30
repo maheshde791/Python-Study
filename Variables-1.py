eecfc8e11b1deeb57c36889237d6914536d6a701
Mahesh Deore <maheshde@cybage.com>
2018-08-30 17:12:12 +0530

    First code variables

---
+++ Variables-1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 30 17:03:19 2018
+
+@author: maheshde
+"""
+
+print("First python code for python variables")
+
+name = "Mahesh";
+print(name)
+print(type(name))
+
+dob=1991
+print(dob)
+print(type(dob))
+
+height=5.9
+print(height)
+print(type(height))
